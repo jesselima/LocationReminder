5eb92f31c49bb37fd392779099a5b74c424f0b1b
Jesse Lima <jesselima.tech@gmail.com>
2021-10-17 12:23:29 -0300

    Setup material, them, colors and styles.

--- starter/app/src/main/java/com/udacity/locationreminder/locationreminders/reminderslist/RemindersListAdapter.kt
+++ starter/app/src/main/java/com/udacity/locationreminder/locationreminders/reminderslist/RemindersListAdapter.kt
@@ -7,5 +7,5 @@ import com.udacity.locationreminder.base.BaseRecyclerViewAdapter
 //Use data binding to show the reminder on the item
 class RemindersListAdapter(callBack: (selectedReminder: ReminderDataItem) -> Unit) :
     BaseRecyclerViewAdapter<ReminderDataItem>(callBack) {
-    override fun getLayoutRes(viewType: Int) = R.layout.it_reminder
+    override fun getLayoutRes(viewType: Int) = R.layout.item_list_reminder
 }
