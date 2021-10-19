fd115d67cd395ab69491f7da3d1f1fdde3f6886f
Jesse Lima <jesselima.tech@gmail.com>
2021-10-19 07:34:50 -0300

    Changed: Ui dialogs extensions improvements.

--- starter/app/src/main/java/com/udacity/locationreminder/utils/ContextExtensions.kt
+++ starter/app/src/main/java/com/udacity/locationreminder/utils/ContextExtensions.kt
@@ -1,55 +1,8 @@
 package com.udacity.locationreminder.utils
 
-import android.Manifest
-import android.app.Activity
 import android.content.Context
-import android.content.pm.PackageManager
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import com.udacity.locationreminder.R
-import com.udacity.locationreminder.databinding.LayoutCustomToastBinding
-
-private const val TOAST_Y_OFF_SET = 250
-private const val TOAST_X_OFF_SET = 0
-
-fun Context.showCustomToast(
-    toastType: ToastType = ToastType.SUCCESS,
-    stringResId: Int = R.string.label_success,
-    durationToast: Int = Toast.LENGTH_SHORT
-) {
-
-    val binding = LayoutCustomToastBinding.inflate(LayoutInflater.from(this))
-
-    binding.customText.text = getString(stringResId)
-
-    val imageResId = when (toastType) {
-        ToastType.ERROR -> R.drawable.ic_error
-        ToastType.INFO -> R.drawable.ic_info
-        ToastType.WARNING -> R.drawable.ic_warning
-        else -> {
-            R.drawable.ic_success
-        }
-    }
-    binding.toastImage.setImageResource(imageResId)
-
-    with (Toast(applicationContext)) {
-        setGravity(Gravity.BOTTOM, TOAST_X_OFF_SET, TOAST_Y_OFF_SET)
-        duration = durationToast
-        view = binding.root
-        show()
-    }
-}
-
-enum class ToastType {
-    SUCCESS,
-    ERROR,
-    INFO,
-    WARNING
-}
 
 @Suppress("DEPRECATION")
 fun Context.isConnected(): Boolean {
--- starter/app/src/main/java/com/udacity/locationreminder/utils/UIDialogsExtensions.kt
+++ starter/app/src/main/java/com/udacity/locationreminder/utils/UIDialogsExtensions.kt
@@ -1,8 +1,12 @@
 package com.udacity.locationreminder.utils
 
 import android.content.Context
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.widget.Toast
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.udacity.locationreminder.R
+import com.udacity.locationreminder.databinding.LayoutCustomToastBinding
 
 fun Context.showDialog(
     context: Context,
@@ -18,4 +22,42 @@ fun Context.showDialog(
             positiveButtonAction?.invoke()
         }
         .show()
+}
+
+fun Context.showCustomToast(
+    toastType: ToastType = ToastType.SUCCESS,
+    stringResId: Int = R.string.label_success,
+    durationToast: Int = Toast.LENGTH_SHORT,
+    offSetY: Int = 250,
+    offSetX: Int = 0,
+    gravity: Int = Gravity.BOTTOM
+) {
+
+    val binding = LayoutCustomToastBinding.inflate(LayoutInflater.from(this))
+
+    binding.customText.text = getString(stringResId)
+
+    val imageResId = when (toastType) {
+        ToastType.ERROR -> R.drawable.ic_error
+        ToastType.INFO -> R.drawable.ic_info
+        ToastType.WARNING -> R.drawable.ic_warning
+        else -> {
+            R.drawable.ic_success
+        }
+    }
+    binding.toastImage.setImageResource(imageResId)
+
+    with (Toast(applicationContext)) {
+        setGravity(gravity, offSetX, offSetY)
+        duration = durationToast
+        view = binding.root
+        show()
+    }
+}
+
+enum class ToastType {
+    SUCCESS,
+    ERROR,
+    INFO,
+    WARNING
 }
