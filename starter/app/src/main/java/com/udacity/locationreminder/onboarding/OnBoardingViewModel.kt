7c7e55cb4f4352a708288433ec4fd433243e0943
Jesse Lima <jesselima.tech@gmail.com>
2021-10-24 02:34:37 -0300

    Fix onBoarding resource references.

--- starter/app/src/main/java/com/udacity/locationreminder/onboarding/OnBoardingViewModel.kt
+++ starter/app/src/main/java/com/udacity/locationreminder/onboarding/OnBoardingViewModel.kt
@@ -22,17 +22,17 @@ class OnBoardingViewModel : ViewModel() {
         OnBoardingStep(
             title = R.string.onboard_step_1_title,
             description = R.string.onboard_step_1_description,
-            animationResId = R.raw.location_pin,
+            animationResId = R.raw.animation_location_pin,
         ),
         OnBoardingStep(
             title = R.string.onboard_step_2_title,
             description = R.string.onboard_step_2_description,
-            animationResId = R.raw.location_select,
+            animationResId = R.raw.animation_location_select,
         ),
         OnBoardingStep(
             title = R.string.onboard_step_3_title,
             description = R.string.onboard_step_3_description,
-            animationResId = R.raw.location_pin_alert,
+            animationResId = R.raw.animation_location_pin_alert,
         ),
     )
 
