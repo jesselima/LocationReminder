f20529c0353d1342a426e9eb52d5375fbb7b7bb0
Jesse Lima <jesselima.tech@gmail.com>
2021-11-02 20:14:30 -0300

    Changed: Improve validateAuthentication extension (replace toast).

--- app/src/main/java/com/udacity/locationreminder/utils/AuthenticationExtensions.kt
+++ app/src/main/java/com/udacity/locationreminder/utils/AuthenticationExtensions.kt
@@ -1,17 +1,20 @@
 package com.udacity.locationreminder.utils
 
 import android.content.Intent
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
+import com.udacity.locationreminder.R
 import com.udacity.locationreminder.authentication.AuthenticationActivity
 
 fun AppCompatActivity.validateAuthentication() {
     FirebaseAuth.getInstance().run {
         if (this.currentUser == null) {
-            Toast.makeText(applicationContext, "Authentication required!", Toast.LENGTH_SHORT).show()
+            showCustomToast(
+                titleResId = R.string.message_authentication_required,
+                toastType = ToastType.WARNING
+            )
             startActivity(Intent(applicationContext, AuthenticationActivity::class.java).apply {
                 flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
             })
