299e799398ce0da99e5ecafd06f4c8f834dde8b3
Jesse Lima <jesselima.tech@gmail.com>
2021-11-03 07:19:12 -0300

    Changed: ReminderBroadcastReceiver improvements

--- app/src/main/java/com/udacity/locationreminder/locationreminders/geofence/GeofenceBroadcastReceiver.kt
+++ app/src/main/java/com/udacity/locationreminder/locationreminders/geofence/GeofenceBroadcastReceiver.kt
@@ -3,6 +3,7 @@ package com.udacity.locationreminder.locationreminders.geofence
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import androidx.core.os.bundleOf
 import com.google.android.gms.location.Geofence
 import com.google.android.gms.location.GeofencingEvent
@@ -13,7 +14,11 @@ import com.udacity.locationreminder.utils.ToastType
 import com.udacity.locationreminder.utils.showCustomToast
 import com.udacity.locationreminder.utils.showOrUpdateNotification
 
+private const val EMPTY = ""
+
 class GeofenceBroadcastReceiver : BroadcastReceiver() {
+
+    private val tag = GeofenceBroadcastReceiver::class.java.simpleName
 
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == ReminderDescriptionActivity.ACTION_GEOFENCE_EVENT) {
@@ -22,10 +27,9 @@ class GeofenceBroadcastReceiver : BroadcastReceiver() {
 
             if (geofencingEvent.hasError()) {
                 val errorMessage = handleGeofenceError(context, geofencingEvent.errorCode)
+                Log.d(tag,"Geofence error: $errorMessage")
                 context.showCustomToast(
-                    titleText = String.format(
-                        context.resources.getString(R.string.geofence_error, errorMessage)
-                    ),
+                    titleText = context.resources.getString(R.string.geofence_error_generic_info),
                     toastType = ToastType.WARNING
                 )
                 return
@@ -36,7 +40,7 @@ class GeofenceBroadcastReceiver : BroadcastReceiver() {
                     context.resources.getString(R.string.notification_text_transition_type_enter)
                 Geofence.GEOFENCE_TRANSITION_EXIT ->
                     context.resources.getString(R.string.notification_text_transition_type_exit)
-                else -> String()
+                else -> EMPTY
             }
 
             if (geofencingEvent.geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ||
@@ -51,7 +55,7 @@ class GeofenceBroadcastReceiver : BroadcastReceiver() {
                                     context.resources.getString(R.string.notification_description),
                                     transition
                                 ),
-                                channelId = ReminderConstants.channelId,
+                                channelId = ReminderConstants.channelIdReminders,
                                 data = bundleOf(
                                     ReminderConstants.argsKeyReminderId to it.requestId.toInt()
                                 )
