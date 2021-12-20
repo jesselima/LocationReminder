9abf2496121265e98ba80ddca502c9a79362f00e
Jesse Lima <jesselima.tech@gmail.com>
2021-12-20 10:19:00 -0300

    Changed: Remove UI Automator test :/

--- app/src/androidTest/java/com/udacity/locationreminder/e2e/ScreenState.kt
+++
@@ -1,9 +0,0 @@
-package com.udacity.locationreminder.e2e
-
-import androidx.test.uiautomator.By
-import androidx.test.uiautomator.UiDevice
-import androidx.test.uiautomator.Until
-
-fun UiDevice.waitUntilScreenLoads(currentAppPackage: String, timeout: Long = 5000) {
-    wait(Until.hasObject(By.pkg(currentAppPackage)), timeout)
-}
